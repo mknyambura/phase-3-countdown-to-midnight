97f4404ec48cca9e9f89c980ab5cb753e38164bd
mknyambura <kariuki.mercie@gmail.com>
2023-02-08 21:39:51 +0300

    completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+    while integer
+        puts "#{integer} SECOND(S)!"
+        integer -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(integer)
+    while integer > 0
+        puts "#{integer} SECONDS!"
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!"
 end
